0897d41b3099b8bb3d061f480904dce98b826d6c
J. O. Makar <jomakar@gmail.com>
2025-04-28 13:42:07 -0400

    basic implementation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # wisdom-of-the-chain
 
 [Wisdom of the Crowd](https://en.wikipedia.org/wiki/Wisdom_of_the_crowd) applied to options chains
+
+Scrape option chain data (to be run every market day): `docker compose run -e TICKERS=... --build run scraper-yahoo-finance`
---
+++ scraper-yahoo-finance.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from requests_ratelimiter import LimiterSession
+import yfinance
+
+import datetime
+import json
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(filename)s:%(lineno)d:%(message)s')
+
+data_path = os.environ.get('DATA_PATH', '/usr/local/share/scraper/data')
+
+# Maximum two requests per five seconds, ref: https://yfinance-python.org/advanced/caching.html
+session = LimiterSession(per_second=2/5.0)
+
+for ticker in os.environ.get('TICKERS', '').split(','):
+    ticker = yfinance.Ticker(ticker, session=session)
+
+    history = ticker.history(period='1d').to_json(orient='split')
+    history_date = datetime.datetime.fromtimestamp(json.loads(history)['index'][0] / 1000).date()
+    with open(f'{data_path}/{ticker.ticker.lower()}-{history_date.strftime("%Y%m%d")}.cjson', 'w') as file:
+        file.write(history + '\n')
+
+        for expiration in ticker.options:
+            file.write(f'{{"type":"call","expiration":"{expiration}",' + \
+                       ticker.option_chain(expiration).calls.to_json(orient='split')[1:] + '\n')
+
+            file.write(f'{{"type":"put","expiration":"{expiration}",' + \
+                       ticker.option_chain(expiration).puts.to_json(orient='split')[1:] + '\n')
+    
+    logging.info(f'{ticker.ticker.lower()} finished')
